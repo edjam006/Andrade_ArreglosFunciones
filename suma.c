24510721f77f8eec114febef4795e906e6750d02
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2023-12-05 20:38:54 -0500

    Funciones listas e integradas al codigo

--- suma.c
+++ suma.c
@@ -15,15 +15,39 @@ void ImprimirMatriz(int a, int b, int Matriz[a][b]) {
         printf("\n");
     }
 }
-int main(int argc, char const *argv[])
-{
+int main(){
+
     srand (time(NULL));
     int a,b;
     printf("Ingrese el numero de filas de la matriz\n"); //Solicitud de numeros de filas 
     scanf("%d", &a);
     printf("Ingrese el numero de columnas de la matriz\n"); //Solicitud de numeros de columnas
     scanf("%d", &b);
+
+    int Matriz2 [a][b], Matriz[a][b], resultado [a][b];
+
+    for(int i = 0; i < a; i++) {
+        for(int j = 0; j < b; j++) {
+            Matriz[i][j] = rand() % 101; //Asignacion de numeros dentro de la matriz
+        }
+    }
     
+    for(int i = 0; i < a; i++) {
+        for(int j = 0; j < b; j++) {
+            Matriz2[i][j] = rand() % 101; 
+        } 
+    }
+    printf("Matriz 1:\n");
+    ImprimirMatriz(a, b, Matriz);
+
+    printf("Matriz 2:\n");
+    ImprimirMatriz(a, b, Matriz2);
+
+    Suma(a,b,Matriz, Matriz2, resultado);
+
+
+    printf("\nSuma de Matrices:\n");
+    ImprimirMatriz(a, b, resultado);
     
 
     return 0;
